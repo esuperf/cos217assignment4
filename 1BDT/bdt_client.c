6fc2562535da4d34b194ecd62c6a4e719b6af5ba
esuperf <es1180@princeton.edu>
2025-11-11 22:45:33 -0500

    debugging

--- 1BDT/bdt_client.c
+++ 1BDT/bdt_client.c
@@ -136,6 +136,7 @@ int main(void) {
   free(temp);
   assert(BDT_insert("a/x") == SUCCESS);
   assert((temp = BDT_toString()) != NULL);
+  fprintf(stderr, "Checkpoint Promotion:\n%s\n", temp);
   assert(!strcmp(temp,"a\na/y\na/x\n"));
   free(temp);
   assert(BDT_rm("a/y") == SUCCESS);
@@ -145,7 +146,7 @@ int main(void) {
   assert(BDT_insert("a/y2") == SUCCESS);
   assert((temp = BDT_toString()) != NULL);
   assert(!strcmp(temp,"a\na/x\na/y2\n"));
-  free(temp);
+  free(temp);  
   assert(BDT_rm("a/y2") == SUCCESS);
   assert((temp = BDT_toString()) != NULL);
   assert(!strcmp(temp,"a\na/x\n"));
