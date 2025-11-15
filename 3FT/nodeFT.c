8c3b9e5a088c22d5da43c45715d900ce4cecbef0
esuperf <es1180@princeton.edu>
2025-11-15 14:45:02 -0500

    w

--- 3FT/nodeFT.c
+++ 3FT/nodeFT.c
@@ -73,3 +73,234 @@ static int Node_compareString(const Node_T oNFirst,
 
    return Path_compareString(oNFirst->oPPath, pcSecond);
 }
+
+/*
+  Creates a new node with path oPPath and parent oNParent.  Returns an
+  int SUCCESS status and sets *poNResult to be the new node if
+  successful. Otherwise, sets *poNResult to NULL and returns status:
+  * MEMORY_ERROR if memory could not be allocated to complete request
+  * CONFLICTING_PATH if oNParent's path is not an ancestor of oPPath
+  * NO_SUCH_PATH if oPPath is of depth 0
+                 or oNParent's path is not oPPath's direct parent
+                 or oNParent is NULL but oPPath is not of depth 1
+  * ALREADY_IN_TREE if oNParent already has a child with this path
+*/
+/*new: cannot create the node if oNParent is a file, or oPPath contains a file*/
+int Node_new(Path_T oPPath, Node_T oNParent, Node_T *poNResult) {
+   struct node *psNew;
+   Path_T oPParentPath = NULL;
+   Path_T oPNewPath = NULL;
+   size_t ulParentDepth;
+   size_t ulIndex;
+   int iStatus;
+
+   assert(oPPath != NULL);
+   assert(oNParent == NULL || CheckerDT_Node_isValid(oNParent));
+   
+   /*checks if oNParent is a file*/
+   if (oNParent->isFile == 1){
+    return NOT_A_DIRECTORY;
+   }
+   /* allocate space for a new node */
+   psNew = malloc(sizeof(struct node));
+   if(psNew == NULL) {
+      *poNResult = NULL;
+      return MEMORY_ERROR;
+   }
+
+   /* set the new node's path */
+   iStatus = Path_dup(oPPath, &oPNewPath);
+   if(iStatus != SUCCESS) {
+      free(psNew);
+      *poNResult = NULL;
+      return iStatus;
+   }
+   psNew->oPPath = oPNewPath;
+
+   /* validate and set the new node's parent */
+   if(oNParent != NULL && oNParent->isFile != 1) {
+      size_t ulSharedDepth;
+
+      oPParentPath = oNParent->oPPath;
+      ulParentDepth = Path_getDepth(oPParentPath);
+      ulSharedDepth = Path_getSharedPrefixDepth(psNew->oPPath,
+                                                oPParentPath);
+      /* parent must be an ancestor of child */
+      if(ulSharedDepth < ulParentDepth) {
+         Path_free(psNew->oPPath);
+         free(psNew);
+         *poNResult = NULL;
+         return CONFLICTING_PATH;
+      }
+
+      /* parent must be exactly one level up from child */
+      if(Path_getDepth(psNew->oPPath) != ulParentDepth + 1) {
+         Path_free(psNew->oPPath);
+         free(psNew);
+         *poNResult = NULL;
+         return NO_SUCH_PATH;
+      }
+
+      /* parent must not already have child with this path */
+      if(Node_hasChild(oNParent, oPPath, &ulIndex)) {
+         Path_free(psNew->oPPath);
+         free(psNew);
+         *poNResult = NULL;
+         return ALREADY_IN_TREE;
+      }
+   }
+   else {
+      /* new node must be root */
+      /* can only create one "level" at a time */
+      if(Path_getDepth(psNew->oPPath) != 1) {
+         Path_free(psNew->oPPath);
+         free(psNew);
+         *poNResult = NULL;
+         return NO_SUCH_PATH;
+      }
+      /*insert something about files??*/
+   }
+   psNew->oNParent = oNParent;
+
+   /* initialize the new node */
+   psNew->oDChildren = DynArray_new(0);
+   if(psNew->oDChildren == NULL) {
+      Path_free(psNew->oPPath);
+      free(psNew);
+      *poNResult = NULL;
+      return MEMORY_ERROR;
+   }
+
+   /* Link into parent's children list */
+   if(oNParent != NULL) {
+      iStatus = Node_addChild(oNParent, psNew, ulIndex);
+      if(iStatus != SUCCESS) {
+         Path_free(psNew->oPPath);
+         free(psNew);
+         *poNResult = NULL;
+         return iStatus;
+      }
+   }
+
+   *poNResult = psNew;
+
+   assert(oNParent == NULL || CheckerDT_Node_isValid(oNParent));
+   assert(CheckerDT_Node_isValid(*poNResult));
+
+   return SUCCESS;
+}
+
+/* should also remove contents if file?*/
+size_t Node_free(Node_T oNNode) {
+   size_t ulIndex;
+   size_t ulCount = 0;
+
+   assert(oNNode != NULL);
+   assert(CheckerDT_Node_isValid(oNNode));
+
+   /* remove from parent's list */
+   if(oNNode->oNParent != NULL) {
+      if(DynArray_bsearch(
+            oNNode->oNParent->oDChildren,
+            oNNode, &ulIndex,
+            (int (*)(const void *, const void *)) Node_compare)
+        )
+         (void) DynArray_removeAt(oNNode->oNParent->oDChildren,
+                                  ulIndex);
+   }
+
+   if(oNNode->contents != NULL){
+    oNNode->contents = NULL;
+   }
+
+   /* recursively remove children */
+   while(DynArray_getLength(oNNode->oDChildren) != 0) {
+      ulCount += Node_free(DynArray_get(oNNode->oDChildren, 0));
+   }
+   DynArray_free(oNNode->oDChildren);
+
+   /* remove path */
+   Path_free(oNNode->oPPath);
+
+   /* finally, free the struct node */
+   free(oNNode);
+   ulCount++;
+   return ulCount;
+}
+
+/*no change*/
+Path_T Node_getPath(Node_T oNNode) {
+   assert(oNNode != NULL);
+
+   return oNNode->oPPath;
+}
+
+/*check if file*/
+boolean Node_hasChild(Node_T oNParent, Path_T oPPath,
+                         size_t *pulChildID) {
+   assert(oNParent != NULL);
+   assert(oPPath != NULL);
+   assert(pulChildID != NULL);
+   assert(oNParent->isFile != 1);
+
+   /* *pulChildID is the index into oNParent->oDChildren */
+   return DynArray_bsearch(oNParent->oDChildren,
+            (char*) Path_getPathname(oPPath), pulChildID,
+            (int (*)(const void*,const void*)) Node_compareString);
+}
+
+/*check if file*/
+size_t Node_getNumChildren(Node_T oNParent) {
+   assert(oNParent != NULL);
+   assert(oNParent->isFile != 1);
+   return DynArray_getLength(oNParent->oDChildren);
+}
+
+/*check if file*/
+int  Node_getChild(Node_T oNParent, size_t ulChildID,
+                   Node_T *poNResult) {
+
+   assert(oNParent != NULL);
+   assert(poNResult != NULL);
+
+   if(oNParent->isFile == 1){
+    return NOT_A_DIRECTORY;
+   }
+   /* ulChildID is the index into oNParent->oDChildren */
+   if(ulChildID >= Node_getNumChildren(oNParent)) {
+      *poNResult = NULL;
+      return NO_SUCH_PATH;
+   }
+   else {
+      *poNResult = DynArray_get(oNParent->oDChildren, ulChildID);
+      return SUCCESS;
+   }
+}
+
+/*no change*/
+Node_T Node_getParent(Node_T oNNode) {
+   assert(oNNode != NULL);
+
+   return oNNode->oNParent;
+}
+
+/*no change*/
+int Node_compare(Node_T oNFirst, Node_T oNSecond) {
+   assert(oNFirst != NULL);
+   assert(oNSecond != NULL);
+
+   return Path_comparePath(oNFirst->oPPath, oNSecond->oPPath);
+}
+
+/*perhaps change later*/
+char *Node_toString(Node_T oNNode) {
+   char *copyPath;
+
+   assert(oNNode != NULL);
+
+   copyPath = malloc(Path_getStrLength(Node_getPath(oNNode))+1);
+   if(copyPath == NULL)
+      return NULL;
+   else
+      return strcpy(copyPath, Path_getPathname(Node_getPath(oNNode)));
+}
